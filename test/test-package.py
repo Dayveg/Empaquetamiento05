1f3a5f10ac2ee955ce420d6c7cb13ee499220f06
Dayana Vega <ld.vega@unaindes.edu.co>
2026-08-31 19:49:52 -0500

    feat: add integration test script for model prediction functionality

--- test/test-package.py
+++ test/test-package.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from model.predict import make_prediction
 
-sample_input_data = pd.read_csv("~empaquetamiento05/test/bankchurn_test.csv")
+sample_input_data = pd.read_csv("/home/ubuntu/Empaquetamiento05/test/bankchurn_test.csv")
 result = make_prediction(input_data=sample_input_data)
 print(result)
